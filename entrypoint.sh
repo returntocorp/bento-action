2039687bbf3ba1a3bb7484d674a7ecc0a1cabf37
Bence Nagy <bence@underyx.me>
2020-03-02 17:58:12 +0100

    Refactor entrypoint to support pushes and be faster on PRs
    Closes https://github.com/returntocorp/bento-core/issues/754

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,31 +5,92 @@ shopt -s nullglob globstar
 bento_path=$(which bento)
 
 bento() {
-    $bento_path --agree --email "$INPUT_ACCEPTTERMSWITHEMAIL" "$@"
+  $bento_path --agree --email "${INPUT_ACCEPTTERMSWITHEMAIL}" "$@"
 }
 
-echo "action's environment: $(bento --version), $(python --version), $(docker --version)"
+handle_pull_request() {
+  # the github ref would be `refs/pull/<pr #>/merge` which isn't known by name here
+  # the github sha seems to refer to the base on re-runs
+  # so we keep our own head ref around
+  real_head_sha=$(git rev-parse HEAD)
 
-echo
-echo "== [1/4] going to go back to the commit you based your pull request on…"
-echo
-git revert HEAD --mainline 1 --no-commit
-git reset .
+  echo
+  echo "== [1/3] going to go back to the commit you based your pull request on…"
+  echo
+  git checkout "${GITHUB_BASE_REF}"
+  git status --branch --short
 
-echo
-echo "== [2/4] …checking for issues present in the codebase before your pull request…"
-echo
-bento init &> /dev/null
-bento archive --all
-git add -f .bento*
+  echo
+  echo "== [2/3] …now adding your pull request's changes back…"
+  echo
 
-echo
-echo "== [3/4] …now adding your pull request's changes back…"
-echo
-git checkout .
-echo "pull request changes restored via git checkout"
+  git checkout "${real_head_sha}" -- .
+  git status --branch --short
 
-echo
-echo "== [4/4] …and see if there are any new findings!"
-echo
-bento check --all
+  echo
+  echo "== [3/3] …and seeing if there are any new findings!"
+  echo
+  bento init &> /dev/null
+  bento check
+}
+
+handle_push() {
+  echo
+  echo "== seeing if there are any findings"
+  echo
+  bento init &> /dev/null
+  bento check --all
+}
+
+handle_unknown() {
+  echo "== [ERROR] the Bento Check action was triggered by an unsupported GitHub event."
+  echo
+  echo "This error is often caused by an unsupported value for `on:` in the action's configuration."
+  echo "To resolve this issue, please confirm that the `on:` key only contains values from the following list: [pull_request, push]."
+  echo "If the problem persists, please file an issue at https://github.com/returntocorp/bento/issues/new/choose"
+  exit 2
+}
+
+check_prerequisites() {
+  if ! [[ -v GITHUB_ACTIONS ]]
+  then
+    echo "== [WARNING] this script is designed to run via GitHub Actions"
+  fi
+
+  if ! [[ -v INPUT_ACCEPTTERMSWITHEMAIL ]]
+  then
+    echo "== [ERROR] you must accept the Bento Terms of Service in the workflow configuration, like so:"
+    echo
+    echo ".github/workflows/bento.yml"
+    echo "---------------------------"
+    echo
+    echo "      [...]"
+    echo "      uses: returntocorp/bento-action@v1"
+    echo "      with:"
+    echo "        acceptTermsWithEmail: <add your email here>"
+    exit 2
+  fi
+}
+
+main() {
+  echo "== action's environment: $(bento --version), $(python --version), $(docker --version)"
+  echo "== triggered by a ${GITHUB_EVENT_NAME}"
+
+  check_prerequisites
+  
+  case ${GITHUB_EVENT_NAME} in
+    pull_request)
+      handle_pull_request
+      ;;
+
+    push)
+      handle_push
+      ;;
+
+    *)
+      handle_unknown
+      ;;
+  esac
+}
+
+main
